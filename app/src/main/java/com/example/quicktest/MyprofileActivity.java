80b811cf05729bb4f28584ce7947d2ec157b5380
Kiet <kietnguyen20101010@gmail.com>
2020-11-08 14:12:28 +0700

    add myprofileactivity

---
+++ app/src/main/java/com/example/quicktest/MyprofileActivity.java
@@ -0,0 +1,14 @@
+package com.example.quicktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyprofileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myprofile);
+    }
+}
