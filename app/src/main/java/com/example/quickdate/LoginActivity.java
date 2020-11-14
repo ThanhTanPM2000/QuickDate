eeda950bbb829768212addf8a4b86d60729a079e
Nguyen Thanh Tan <thanhtanpm2000@gmail.com>
2020-11-15 02:52:08 +0700

    update gradle, modified strings.xml

--- app/src/main/java/com/example/quickdate/LoginActivity.java
+++ app/src/main/java/com/example/quickdate/LoginActivity.java
@@ -1,19 +1,30 @@
 package com.example.quickdate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText username, password;
     CheckBox rememberpass;
     TextView btnLogin, btnforgotpass, btnSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +32,31 @@ public class LoginActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("something ", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("something", "Error adding document", e);
+                    }
+                });
+
     }
 
     @Override
--- app/src/main/java/com/example/quickdate/MainActivity.java
+++ app/src/main/java/com/example/quickdate/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.quickdate;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.quickdate.Models.Users;
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
@@ -24,8 +26,11 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
@@ -33,10 +38,21 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.firestore.Transaction;
+import com.google.firebase.firestore.WriteBatch;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
@@ -46,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener authStateListener;
     DatabaseReference reference;
     private static final String TAG = "FacebookAuthentication";
-
     private Button btnLogin, btnFacebook, btnSignup;
 
     @Override
@@ -126,7 +141,8 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                            /*FirebaseUser user = firebaseAuth.getCurrentUser();
 
                             reference = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
 
@@ -138,14 +154,64 @@ public class MainActivity extends AppCompatActivity {
                             reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
+                                    if (task.isSuccessful()) {
                                         Intent a = new Intent(getBaseContext(), Profile_User.class);
                                         a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                                         startActivity(a);
                                         finish();
                                     }
                                 }
+                            });*/
+                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                            /*final DocumentReference doc = db.collection("cities").document("test");
+
+                            db.runTransaction(new Transaction.Function<Double>()
+                            {
+                                @Nullable
+                                @Override
+                                public Double apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
+                                    DocumentSnapshot snapshot = transaction.get(doc);
+
+                                    double newvalue = snapshot.getDouble(("listExample"))+2;
+                                    if(newvalue>=100){
+                                        transaction.update(doc, "listExample", newvalue);
+                                        return newvalue;
+                                    }
+                                    else
+                                    {
+                                        throw new FirebaseFirestoreException("listExample is too low than 100", FirebaseFirestoreException.Code.ABORTED);
+                                    }
+                                }
+                            }).addOnSuccessListener(new OnSuccessListener<Double>() {
+                                @Override
+                                public void onSuccess(Double aDouble) {
+
+                                }
+                            });*/
+                            /*WriteBatch wb = db.batch();
+
+                            DocumentReference doc = db.collection("cities").document("Ho Chi Minh");
+                            wb.set(doc, new Users());
+
+                            DocumentReference doc2 = db.collection("cities").document("test");
+                            wb.delete(doc2);
+
+                            wb.commit().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                }
+                            });*/
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("banh", FieldValue.delete());
+                            db.collection("cities").document("Ho Chi Minh").update(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+
+                                }
                             });
+
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
@@ -168,13 +234,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        firebaseUser = firebaseAuth.getInstance().getCurrentUser();
+        /*firebaseUser = firebaseAuth.getInstance().getCurrentUser();
         firebaseAuth.addAuthStateListener(authStateListener);
         if(firebaseUser != null){
             Intent intent = new Intent(getBaseContext(), Profile_User.class);
             startActivity(intent);
             finish();
-        }
+        }*/
     }
 
     @Override
